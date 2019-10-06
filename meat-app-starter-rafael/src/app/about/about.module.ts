5abebd95ad7a313c8d5d38fb33739b45bd23050b
Rafael Rodrigues Pereira <rafaelrpdev@gmail.com>
2019-10-06 12:39:40 -0300

    adjust the about module declaration

--- meat-app-starter-rafael/src/app/about/about.module.ts
+++ meat-app-starter-rafael/src/app/about/about.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about.component';
 
-const ROUTES: Routes = [
-  { path: '', component: AboutComponent }
-];
+const ROUTES: Routes = [{path: '', component: AboutComponent}]
 
 @NgModule({
   declarations: [AboutComponent],
